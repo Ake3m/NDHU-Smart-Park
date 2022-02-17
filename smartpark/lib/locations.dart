1486e905e46b853df1d995549ec7f362c42aa12b
Akeem Peters <akeemp13@gmail.com>
2022-02-17 13:51:18 +0800

    colors added

--- smartpark/lib/locations.dart
+++ smartpark/lib/locations.dart
@@ -11,12 +11,18 @@ class LocationTile {
   LocationTile(this.tileTitle, this.imgUrl, this.tileColor);
 
   List<LocationTile> parkingLocations = [
-    LocationTile('test', './assets/images/wip.png', Colors.red),
+    LocationTile('test', './assets/images/wip.png',
+        const Color.fromARGB(255, 9, 228, 228)),
     LocationTile(
-        'Administration Building', './assets/images/wip.png', Colors.red),
+        'Administration Building',
+        './assets/images/administration.png',
+        const Color.fromARGB(255, 47, 49, 139)),
     LocationTile(
-        'Science and Engineering 2', './assets/images/wip.png', Colors.red),
-    LocationTile('Sports Center', './assets/images/wip.png', Colors.red),
+        'Science and Engineering 2',
+        './assets/images/science_and_engineering.png',
+        const Color.fromARGB(255, 249, 217, 51)),
+    LocationTile('Sports Center', './assets/images/sports_center.png',
+        const Color.fromARGB(255, 203, 6, 85)),
     LocationTile('Coming Soon', './assets/images/wip.png', Colors.red)
   ];
 }
