e7544fb3855b04b531a85ad3cfa5f1ba1687c032
Akeem Peters <akeemp13@gmail.com>
2022-03-24 14:54:43 +0800

    added create function

--- ParkingLotMonitor/parkinglotsystem.py
+++ ParkingLotMonitor/parkinglotsystem.py
@@ -1,35 +1,46 @@
 import os
 from pathlib import Path
 import cv2 as cv
-import json
-from cv2 import EVENT_LBUTTONDOWN
-from firebase_admin import credentials, initialize_app, storage
+import numpy as np
+from firebase_admin import initialize_app
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import storage
 
+
+#GLOBAL VARIABLES SECTION
 drawing = False
-ix = 0
-iy = 0
+ix = 0 #initial x
+iy = 0 #initial y
+ex=0 #ending x
+ey=0 #ending y
 sample_img = ""
-sample_points = []
-position_list = []
+imgCopy=""
+sample_points = [] #list holding the sample points 
+position_list = [] #list holding the position of each lot
 width=0
 height=0
 
+#FIREBASE CONFIGURATION SETUP
+cred=credentials.Certificate("./serviceAccountKey.json")
+initialize_app(cred, {'storageBucket': 'smart-park-13acd.appspot.com'})
+database=firestore.client()
 
 def draw(event, x, y, flags, parameters):
-    global ix, iy, drawing
+    global ix, iy, drawing, ex, ey
     if event == cv.EVENT_LBUTTONDOWN:
-        drawing = True
         ix = x
         iy = y
+        drawing = True
     if event == cv.EVENT_MOUSEMOVE:
         if drawing == True:
-            cv.rectangle(sample_img, (ix, iy), (x, y), (0, 255, 0), -1)
+            cv.rectangle(imgCopy, (ix, iy), (x, y), (0, 255, 0), -1)
     if event == cv.EVENT_LBUTTONUP:
         drawing = False
-        sample_points.append(ix)
-        sample_points.append(iy)
-        sample_points.append(x)
-        sample_points.append(y)
+        ex=x
+        ey=y
+        # sample_points.extend([ix,iy,x,y])
+        
 
 
 def outlineParkingSpace(events, x, y, flags, parameters):
@@ -42,13 +53,21 @@ def outlineParkingSpace(events, x, y, flags, parameters):
                 position_list.pop(i)
 
 
-def drawSample(img):
+def drawSample(selected):
+    global imgCopy
     print("Drag the mouse over the area of 1 parking space.")
+    print("If you are satisfied, press \'s\' to save sample.")
+    print("if you want to redo, press \'r\'.")
+    imgCopy=cv.imread('./ParkingLotMonitor/Samples/{}'.format(selected),1)
     while True:
-        cv.imshow("Draw sample", img)
+        cv.imshow("Draw sample", imgCopy)
         cv.setMouseCallback("Draw sample", draw)
-        if cv.waitKey(20) & 0xFF == ord('q'):
+        k=cv.waitKey(1)
+        if k==ord('s'):
+            sample_points.extend([ix,iy,ex,ey])
             break
+        elif k==ord('r'):
+            imgCopy=cv.imread('./ParkingLotMonitor/Samples/{}'.format(selected),1)
     cv.destroyAllWindows()
 
 
@@ -62,10 +81,25 @@ def edit():
 
 def create():
     global sample_img
+    global imgCopy
     global width
     global height
+    print("What type of parking lot do you want to create?")
+    print("1.Car\n2.Motorcycle/Scooter")
+    satisfied=False
+    parkinglot_type=""
+    while not satisfied:
+        choice=int(input())
+        if choice == 1 or choice==2:
+            if choice==1:
+                parkinglot_type="Car"
+            else:
+                parkinglot_type="Scooter"
+            satisfied=True
+        else:
+            print("Invalid input. Please try again.")
     print("What would you like to name this parking lot?")
-    parking_lot_name = input()
+    parkinglot_name = input()
     print("In order to create a parking lot, perform the following steps.")
     print("1. Place a sample image of the parking lot in the \"Samples\" folder.\n2. Take a sample of one of the parking spaces.\n3. Outline the parking lot by clicking the top left of each lot.\n\t -Left click to add\n\t -Right click to remove\n4. Press \"s\" to save, \"q\" to quit.")
     print("Is the sample image already in the folder? (Y/N)")
@@ -81,20 +115,36 @@ def create():
             print("{}.{}".format(counter, sample.capitalize()))
         selection = int(input())
         selected_img = sample_images[selection-1]
-        sample_img = cv.imread(
-            "./ParkingLotMonitor/Samples/{}".format(selected_img))
-        drawSample(sample_img)
+        drawSample(selected_img)
         width = sample_points[2]-sample_points[0]
         height = sample_points[3]-sample_points[1]
-        print("Select the top left corner of each lot")
+        print("Left click the top left corner of each lot to outline")
+        print("Right click anywhere in the outline to remove it.")
+        print("Press \'s\' to save.")
         while True:
+            sample_img = cv.imread(
+            "./ParkingLotMonitor/Samples/{}".format(selected_img))
             for position in position_list:
                 cv.rectangle(sample_img, tuple(position), (position[0]+width, position[1]+height), (0,255,0),3)
             cv.imshow("Outline Parking Lot", sample_img)
             cv.setMouseCallback("Outline Parking Lot", outlineParkingSpace)
             k=cv.waitKey(1)
-            if k ==ord('q'):
+            if k ==ord('s'):
+                cv.destroyAllWindows()
                 break
+        print("Lastly, how many lots per row are there?")
+        lots_per_row=int(input())
+        vacant_lots=[True for lot in position_list]
+        data={
+            "name": parkinglot_name,
+            "capacity":len(vacant_lots),
+            "lots": vacant_lots,
+            "lotsPerRow": lots_per_row
+        }
+        
+        db_ref=database.collection(parkinglot_type).document(parkinglot_name).set(data)
+        print("Parking lot sucessfully added to database.")
+        
 
 
 def main():
