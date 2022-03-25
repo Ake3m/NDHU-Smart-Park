ca32f65a06f22b78307033283113a6e2a7ddc11a
Akeem Peters <akeemp13@gmail.com>
2022-03-25 11:21:34 +0800

    edit function added

--- ParkingLotManager/parkinglotsystem.py
+++ ParkingLotManager/parkinglotsystem.py
@@ -1,6 +1,7 @@
 from gc import collect
 import os
 from pathlib import Path
+from re import X
 import cv2 as cv
 import numpy as np
 from firebase_admin import initialize_app
@@ -11,27 +12,28 @@ from firebase_admin import storage
 # from ParkingLotManager.parkinglotmanager import checkParkingSpaces
 
 
-#GLOBAL VARIABLES SECTION
+# GLOBAL VARIABLES SECTION
 drawing = False
-ix = 0 #initial x
-iy = 0 #initial y
-ex=0 #ending x
-ey=0 #ending y
+ix = 0  # initial x
+iy = 0  # initial y
+ex = 0  # ending x
+ey = 0  # ending y
 sample_img = ""
-imgCopy=""
-sample_points = [] #list holding the sample points 
-position_list = [] #list holding the position of each lot
-width=0
-height=0
-lot_names=[]
-lot_types=[]
-vacant_lots=[]
-previous_list=[]
+imgCopy = ""
+sample_points = []  # list holding the sample points
+position_list = []  # list holding the position of each lot
+width = 0
+height = 0
+lot_names = []
+lot_types = []
+vacant_lots = []
+previous_list = []
 
-#FIREBASE CONFIGURATION SETUP
-cred=credentials.Certificate("./serviceAccountKey.json")
+# FIREBASE CONFIGURATION SETUP
+cred = credentials.Certificate("./serviceAccountKey.json")
 initialize_app(cred, {'storageBucket': 'smart-park-13acd.appspot.com'})
-database=firestore.client()
+database = firestore.client()
+
 
 def draw(event, x, y, flags, parameters):
     global ix, iy, drawing, ex, ey
@@ -44,19 +46,23 @@ def draw(event, x, y, flags, parameters):
             cv.rectangle(imgCopy, (ix, iy), (x, y), (0, 255, 0), -1)
     if event == cv.EVENT_LBUTTONUP:
         drawing = False
-        ex=x
-        ey=y
+        ex = x
+        ey = y
         # sample_points.extend([ix,iy,x,y])
-        
 
 
 def outlineParkingSpace(events, x, y, flags, parameters):
     if events == cv.EVENT_LBUTTONDOWN:  # if left click area, add that point to the list
         position_list.append((x, y))
+        print("Adding...")
+        print(position_list)
     if events == cv.EVENT_RBUTTONDOWN:  # if right click, remove point from list
+        print("Removing...")
+        print(position_list)
         for i, position in enumerate(position_list):
             x1, y1 = position
             if x1 < x < x1+width and y1 < y < y1+height:
+                print("Yes")
                 position_list.pop(i)
 
 
@@ -65,26 +71,30 @@ def drawSample(selected):
     print("Drag the mouse over the area of 1 parking space.")
     print("If you are satisfied, press \'s\' to save sample.")
     print("if you want to redo, press \'r\'.")
-    imgCopy=cv.imread('./ParkingLotManager/Samples/{}'.format(selected),1)
+    imgCopy = cv.imread('./ParkingLotManager/Samples/{}'.format(selected), 1)
     while True:
         cv.imshow("Draw sample", imgCopy)
         cv.setMouseCallback("Draw sample", draw)
-        k=cv.waitKey(1)
-        if k==ord('s'):
-            sample_points.extend([ix,iy,ex,ey])
+        k = cv.waitKey(1)
+        if k == ord('s'):
+            sample_points.extend([ix, iy, ex, ey])
             break
-        elif k==ord('r'):
-            imgCopy=cv.imread('./ParkingLotManager/Samples/{}'.format(selected),1)
+        elif k == ord('r'):
+            imgCopy = cv.imread(
+                './ParkingLotManager/Samples/{}'.format(selected), 1)
     cv.destroyAllWindows()
+
+
 def checkParkingSpaces(image, width, height, position_list):
     global previous_list
 
     for i, position in enumerate(position_list):
-        x,y=position
-        cropped_img=image[y:y+height, x:x+width]
-        #add detection code here
+        x, y = position
+        cropped_img = image[y:y+height, x:x+width]
+        # add detection code here
 
-        cv.rectangle(image, position, (position[0]+width, position[1]+height), (0,255,0), 3)
+        cv.rectangle(image, position,
+                     (position[0]+width, position[1]+height), (0, 255, 0), 3)
 
 
 def monitor():
@@ -93,48 +103,109 @@ def monitor():
     global position_list
     global vacant_lots
     global width, height
-    #retrieve values needed from database
+    # retrieve values needed from database
     print("Which parking lot type would you like to monitor?")
-    collections=database.collections()
-    counter=1
-    lot_types=[]
+    collections = database.collections()
+    counter = 1
+    lot_types = []
     for collection in collections:
         lot_types.append(collection.id)
-        print("{}.{}".format(counter,collection.id))
-        counter+=1
-    collection_choice=lot_types[int(input())-1]
+        print("{}.{}".format(counter, collection.id))
+        counter += 1
+    collection_choice = lot_types[int(input())-1]
     print("Which Parking Lot would you like to monitor?")
-    documents=database.collection(collection_choice).stream()
-    counter=1
+    documents = database.collection(collection_choice).stream()
+    counter = 1
     for doc in documents:
         lot_names.append(doc.id)
-        print("{}.{}".format(counter,doc.id))
-        counter+=1
-    doc_choice=lot_names[int(input())-1]
+        print("{}.{}".format(counter, doc.id))
+        counter += 1
+    doc_choice = lot_names[int(input())-1]
 
-    parking_lot_info=database.collection(collection_choice).document(doc_choice).get().to_dict() #gets the data from database and converts to dictionary
+    parking_lot_info = database.collection(collection_choice).document(
+        doc_choice).get().to_dict()  # gets the data from database and converts to dictionary
     for i in range(len(parking_lot_info["x"])):
-        position_list.append([parking_lot_info["x"][i],parking_lot_info["y"][i]])
-    vacant_lots=parking_lot_info["lot"]
-    width=parking_lot_info["width"]
-    height=parking_lot_info["height"]
-    #access camera and check lot
-    cap=cv.VideoCapture(0) #change to 0 for laptop webcam, 1 for external webcam
+        position_list.append(
+            [parking_lot_info["x"][i], parking_lot_info["y"][i]])
+    vacant_lots = parking_lot_info["lot"]
+    width = parking_lot_info["width"]
+    height = parking_lot_info["height"]
+    # access camera and check lot
+    # change to 0 for laptop webcam, 1 for external webcam
+    cap = cv.VideoCapture(0)
     if not cap.isOpened():
         print("Error opening camera")
         exit()
     while True:
-        flag, img=cap.read()
+        flag, img = cap.read()
         checkParkingSpaces(img, width, height, position_list)
         cv.imshow("Monitor feed", img)
-        k=cv.waitKey(100)
-        if k==ord('q'):
+        k = cv.waitKey(100)
+        if k == ord('q'):
             cv.destroyAllWindows()
             break
 
 
 def edit():
-    pass
+    global width, height
+    # retrieve values needed from database
+    print("Which parking lot type would you like to edit?")
+    collections = database.collections()
+    counter = 1
+    lot_types = []
+    for collection in collections:
+        lot_types.append(collection.id)
+        print("{}.{}".format(counter, collection.id))
+        counter += 1
+    collection_choice = lot_types[int(input())-1]
+    print("Which Parking Lot would you like to edit?")
+    documents = database.collection(collection_choice).stream()
+    counter = 1
+    for doc in documents:
+        lot_names.append(doc.id)
+        print("{}.{}".format(counter, doc.id))
+        counter += 1
+    doc_choice = lot_names[int(input())-1]
+
+    parking_lot_info = database.collection(collection_choice).document(
+        doc_choice).get().to_dict()  # gets the data from database and converts to dictionary
+    for i in range(len(parking_lot_info["x"])):
+        position_list.append(
+            (parking_lot_info["x"][i], parking_lot_info["y"][i]))
+    vacant_lots = parking_lot_info["lot"]
+    width = parking_lot_info["width"]
+    height = parking_lot_info["height"]
+    sample_images = os.listdir("./ParkingLotManager/Samples")
+    print("Please select an image from the folder")
+    counter = 1
+    for sample in sample_images:
+        print("{}.{}".format(counter, sample.capitalize()))
+        counter += 1
+    selection = int(input())
+    selected_img = sample_images[selection-1]
+    while True:
+        sample_img = cv.imread(
+            "./ParkingLotManager/Samples/{}".format(selected_img))
+        for position in position_list:
+            cv.rectangle(sample_img, tuple(
+                position), (position[0]+width, position[1]+height), (0, 255, 0), 3)
+        cv.imshow("Outline Parking Lot", sample_img)
+        cv.setMouseCallback("Outline Parking Lot", outlineParkingSpace)
+        k = cv.waitKey(1)
+        if k == ord('s'):
+            cv.destroyAllWindows()
+            break
+    parking_lot_updated_info=database.collection(collection_choice).document(
+        doc_choice)
+    x_positions = [position[0] for position in position_list]
+    y_positions = [position[1] for position in position_list]
+    vacant_lots = [True for lot in position_list]
+    parking_lot_updated_info.update({
+        "x": x_positions,
+        "y": y_positions,
+        "lot":vacant_lots
+    })
+    print("Information successfully updated")    
 
 
 def create():
@@ -144,16 +215,16 @@ def create():
     global height
     print("What type of parking lot do you want to create?")
     print("1.Car\n2.Motorcycle/Scooter")
-    satisfied=False
-    parkinglot_type=""
+    satisfied = False
+    parkinglot_type = ""
     while not satisfied:
-        choice=int(input())
-        if choice == 1 or choice==2:
-            if choice==1:
-                parkinglot_type="Car"
+        choice = int(input())
+        if choice == 1 or choice == 2:
+            if choice == 1:
+                parkinglot_type = "Car"
             else:
-                parkinglot_type="Scooter"
-            satisfied=True
+                parkinglot_type = "Scooter"
+            satisfied = True
         else:
             print("Invalid input. Please try again.")
     print("What would you like to name this parking lot?")
@@ -171,6 +242,7 @@ def create():
         counter = 1
         for sample in sample_images:
             print("{}.{}".format(counter, sample.capitalize()))
+            counter += 1
         selection = int(input())
         selected_img = sample_images[selection-1]
         drawSample(selected_img)
@@ -181,34 +253,36 @@ def create():
         print("Press \'s\' to save.")
         while True:
             sample_img = cv.imread(
-            "./ParkingLotManager/Samples/{}".format(selected_img))
+                "./ParkingLotManager/Samples/{}".format(selected_img))
             for position in position_list:
-                cv.rectangle(sample_img, tuple(position), (position[0]+width, position[1]+height), (0,255,0),3)
+                cv.rectangle(sample_img, tuple(
+                    position), (position[0]+width, position[1]+height), (0, 255, 0), 3)
             cv.imshow("Outline Parking Lot", sample_img)
             cv.setMouseCallback("Outline Parking Lot", outlineParkingSpace)
-            k=cv.waitKey(1)
-            if k ==ord('s'):
+            k = cv.waitKey(1)
+            if k == ord('s'):
                 cv.destroyAllWindows()
                 break
         print("Lastly, how many lots per row are there?")
-        lots_per_row=int(input())
-        x_positions=[position[0] for position in position_list]
-        y_positions=[position[1] for position in position_list]
-        vacant_lots=[True for lot in position_list]
-        data={
+        lots_per_row = int(input())
+        x_positions = [position[0] for position in position_list]
+        y_positions = [position[1] for position in position_list]
+        vacant_lots = [True for lot in position_list]
+        data = {
             "name": parkinglot_name,
-            "capacity":len(vacant_lots),
+            "capacity": len(vacant_lots),
             "x": x_positions,
             "y": y_positions,
             "lot": vacant_lots,
             "lotsPerRow": lots_per_row,
             "width": width,
-            "height": height
+            "height": height,
+            "imgURL": selected_img
         }
-        
-        db_ref=database.collection(parkinglot_type).document(parkinglot_name).set(data)
+
+        db_ref = database.collection(parkinglot_type).document(
+            parkinglot_name).set(data)
         print("Parking lot sucessfully added to database.")
-        
 
 
 def main():
@@ -223,7 +297,7 @@ def main():
         elif choice == 2:
             create()
         elif choice == 3:
-            pass
+            edit()
         elif choice == 4:
             print("Thank you. Have a nice day")
             exit_option = True
